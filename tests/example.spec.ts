05095f5e2721d1fcac327d9f14416ff8615d4c04
Lucianbt <92264373+Lucianbt@users.noreply.github.com>
2025-10-17 18:12:33 +0300

    start again

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-// tests/example.spec.ts
-import { test, expect } from '@playwright/test';
-
-test('homepage title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
